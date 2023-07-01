65e612f844159b90597f4377bea02c2b493b6a4f
mustafakork <mstfbsrysrykb60@gmail.com>
2023-07-01 21:43:26 +0300

    second

--- src/test/java/tests/day08_ıframe/C05_Actions.java
+++ src/test/java/tests/day08_ıframe/C05_Actions.java
@@ -23,7 +23,7 @@ driver.get("https://the-internet.herokuapp.com/context_menu");
         //4- Alert’te cikan yazinin “You selected a context menu” oldugunu test edin.
         //5- Tamam diyerek alert’i kapatalim
         //6- Elemental Selenium linkine tiklayalim
-        //7- Acilan sayfada h1 taginda “Elemental Selenium” yazdigini te
+        //7- Acilan sayfada h1 taginda “Elemental Selenium” yazdigini test et
 
 
     }
