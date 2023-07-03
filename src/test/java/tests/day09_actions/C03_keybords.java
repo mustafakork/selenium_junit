42e3abec040c7339e644461e27c4da703cb1ee81
mustafakork <mstfbsrysrykb60@gmail.com>
2023-07-03 11:25:20 +0300

    day09d

--- src/test/java/tests/day09_actions/C03_keybords.java
+++ src/test/java/tests/day09_actions/C03_keybords.java
@@ -1,4 +1,42 @@
 package tests.day09_actions;
 
-public class C03_keybords {
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import tests.utilities.Testbase;
+
+public class C03_keybords extends Testbase {
+
+    @Test
+    public void test01(){
+
+
+//1- https://www.facebook.com adresine gidelim
+        driver.get("https://www.facebook.com");
+//2- Yeni hesap olustur butonuna basalim
+       driver.findElement(By.xpath("//*[text()='Yeni hesap oluştur']")).click();
+       bekle(10);
+
+//3- Ad, soyad, mail ve sifre kutularina deger yazalim ve kaydol tusuna basalim
+//4- Kaydol tusuna basalim
+
+
+        Actions actions=new Actions(driver);
+        WebElement adelementi= driver.findElement(By.name("firstname"));
+
+        actions.click(adelementi)
+                .sendKeys("muko")
+                .sendKeys(Keys.TAB)
+                .sendKeys("kork")
+                .sendKeys(Keys.TAB)
+                .sendKeys("asdeff@gmail.com")
+                .sendKeys(Keys.TAB)
+                .sendKeys("asdeff@gmail.com")
+                .sendKeys(Keys.TAB)
+                .sendKeys("1122").perform();
+
+    }
+
 }
