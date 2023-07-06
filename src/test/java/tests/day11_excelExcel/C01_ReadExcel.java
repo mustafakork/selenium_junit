c36920628b3a55033c17dd1d7a9fbc8358c80015
mustafakork <mstfbsrysrykb60@gmail.com>
2023-07-06 10:21:02 +0300

    day12

--- src/test/java/tests/day11_excelExcel/C01_ReadExcel.java
+++ src/test/java/tests/day11_excelExcel/C01_ReadExcel.java
@@ -1,4 +1,16 @@
 package tests.day11_excelExcel;
 
+import org.junit.Test;
+
+import java.io.IOException;
+
 public class C01_ReadExcel {
+
+
+    @Test
+    public void test01() throws IOException {
+
+        String dosyayolu ="src/test/java/tests/day11_excelExcel/ulkeler.xlsx";
+
+    }
 }
